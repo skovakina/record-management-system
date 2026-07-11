e6afb4b1d91454c234a62935eaaf6dd263d75621
Sveta Kovakina <skovakina@gmail.com>
2026-07-11 16:23:13 -0700

    fix: merge

--- src/main.py
+++ src/main.py
@@ -4,17 +4,9 @@ from record.store import RecordStore
 
 def main():
     store = RecordStore()
-<<<<<<< HEAD
-    store.load_records()
-    
-    print("Loaded records")
-    
-    RecordManagerApp().mainloop()
-=======
     records = store.load()
 
     RecordManagerApp(records).mainloop()
->>>>>>> e69420f (connect with ui)
 
 
 if __name__ == "__main__":
